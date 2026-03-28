5417bc78981af8b3a2e218254a3c1332e2e2449d
ntfreddy <18194805+ntfreddy@users.noreply.github.com>
2026-03-27 21:58:27 -0400

    changed name

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hymnos/i);
+  const linkElement = screen.getByText(/Hymnus/i);
   expect(linkElement).toBeInTheDocument();
 });
